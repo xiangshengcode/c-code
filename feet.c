7082e670d316d046dda3ab56695b5d0e7e934c87
曾祥生 <1836405175@qq.com>
2022-10-20 20:57:04 +0800

    c语言第一次上传

---
+++ feet.c
@@ -0,0 +1,13 @@
+// fathm-ft.c
+#include<stdio.h>
+int main()
+{
+	int feet ,fathoms;
+	
+	fathoms=2;
+	feet=6*fathoms;
+	printf("there are %d feet in %d fathoms\n",feet ,fathoms);
+	printf("yes,i said %d feet\n",6*fathoms);
+	
+	return 0;
+ } 
